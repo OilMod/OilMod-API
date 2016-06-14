ad10c7974d7bd5623dc75a386e8531450d021b04
sirati97 <TheSirati97@gmail.com>
2016-06-14 22:44:13 +0200

    bug fix (cannot change name of custom ui)

--- src/main/java/de/sirati97/oilmod/api/userinterface/internal/InterfaceFactory.java
+++ src/main/java/de/sirati97/oilmod/api/userinterface/internal/InterfaceFactory.java
@@ -2,10 +2,12 @@ package de.sirati97.oilmod.api.userinterface.internal;
 
 import de.sirati97.oilmod.api.userinterface.UserInterfaceBuilder;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by sirati97 on 14.06.2016 for OilMod-Api.
  */
 public interface InterfaceFactory {
-    Interface createChestInterace(Player player, UserInterfaceBuilder builder, int rows);
+    Interface createChestInterace(Player player, UserInterfaceBuilder builder, String title, int rows);
+    Interface createChestInterace(Player player, UserInterfaceBuilder builder, String title, int rows, ItemStack background);
 }
